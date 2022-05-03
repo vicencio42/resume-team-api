07062a55119e21e7a272fc64e29db54672f1996c
Vicencio42 <vicencio42@gmail.com>
2022-05-02 20:35:03 -0500

    added student model details

--- db/migrate/20220503003353_create_students.rb
+++ db/migrate/20220503003353_create_students.rb
@@ -1,7 +1,17 @@
 class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
-      
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone_number
+      t.text :short_bio
+      t.string :linkedin_url
+      t.string :twitter_handle
+      t.string :personal_blig
+      t.string :resume
+      t.string :github_url
+      t.string :photo
 
       t.timestamps
     end
