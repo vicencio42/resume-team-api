320ac6b82d8a686e98547eb5806ff1305fabd66e
marocks31 <mollyarisley@gmail.com>
2022-05-02 20:54:59 -0500

    created restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/students" => "students#index"
+  post "/students" => "students#create"
+  get "/students/:id" => "students#show"
+  patch "/students/:id" => "students#update"
+  delete "/students/:id" => "students#destroy"
 end
