94e8da4e4b1dfc33f9639881c2c575e23fdca969
marocks31 <mollyarisley@gmail.com>
2022-05-03 19:40:57 -0500

    created migrations to include student ids in all tables, changed personal blig to personal blog in student table

--- db/migrate/20220504000636_add_student_id.rb
+++ db/migrate/20220504000636_add_student_id.rb
@@ -1,5 +1,12 @@
 class AddStudentId < ActiveRecord::Migration[7.0]
   def change
-    add_column :capstones, 
+    add_column :capstones, :student_id, :integer
+    add_column :educations, :student_id, :integer
+    add_column :experiences, :student_id, :integer
+    add_column :skills, :student_id, :integer
+    
+    remove_column :students, :personal_blig, :string
+    add_column :students, :personal_blog, :string
+
   end
 end
