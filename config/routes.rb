3946b67beab3c602268ca54247751c34ed3509e5
Vicencio42 <vicencio42@gmail.com>
2022-05-03 21:10:36 -0500

    added routes and http req

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   get "/students/:id" => "students#show"
   patch "/students/:id" => "students#update"
   delete "/students/:id" => "students#destroy"
+
+  get "/educations" => "educations#index"
+
+  get "/experiences" => "experiences#index"
+
+  get "/skills" => "skills#index"
+
+  get "/capstones" => "capstones#index"
 end
